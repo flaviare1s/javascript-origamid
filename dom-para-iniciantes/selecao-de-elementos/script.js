883223d222fe843ff50a1aea082075b68cc3c679
Flávia Reis <flaviarster@gmail.com>
2024-02-12 16:35:44 -0300

    Fiz os exercícios de seleção de elementos

--- dom-para-iniciantes/selecao-de-elementos/script.js
+++ dom-para-iniciantes/selecao-de-elementos/script.js
@@ -1,10 +1,20 @@
 // Retorne no console todas as imagens do site
+const imagens = document.querySelectorAll('img')
+console.log(imagens)
 
 // Retorne no console apenas as imagens que começaram com a palavra imagem
+const imagensNome = document.querySelectorAll('img[src^="img/imagem"]')
+console.log(imagensNome)
 
 // Selecione todos os links internos (onde o href começa com #)
 
 // Selecione o primeiro h2 dentro de .animais-descricao
+const primeiroH2 = document.querySelector('.animais-descricao h2')
+console.log(primeiroH2)
 
 // Selecione o último p do site
+const paragrafos = document.querySelectorAll('p')
+const ultimoP = paragrafos[paragrafos.length - 1]
+console.log(ultimoP)
+
 
