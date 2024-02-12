c1b24892e030418ae2384e8ada07e678e8f53d53
Flávia Reis <flaviarster@gmail.com>
2024-02-12 19:39:34 -0300

    Fiz os exercícios de array e arrow function

--- dom-para-iniciantes/forEach-e-ArrowFunction/script.js
+++ dom-para-iniciantes/forEach-e-ArrowFunction/script.js
@@ -1,18 +1,44 @@
 // Mostre no console cada parágrado do site
+const paragrafos = document.querySelectorAll('p')
+const arrayParagrafos = Array.from(paragrafos)
+
+arrayParagrafos.forEach((p) => {
+  console.log(p)
+})
 
 // Mostre o texto dos parágrafos no console
+arrayParagrafos.forEach((p) => {
+  console.log(p.innerText)
+})
 
 // Como corrigir os erros abaixo:
+
+// RESPOSTA: Colocando os parênteses
+
+// const imgs = document.querySelectorAll('img');
+
+// imgs.forEach(item, index => {
+//   console.log(item, index);
+// });
+
+// let i = 0;
+// imgs.forEach( => {
+//   console.log(i++);
+// });
+
+// imgs.forEach(() => i++);
+
+
 const imgs = document.querySelectorAll('img');
 
-imgs.forEach(item, index => {
-  console.log(item, index);
-});
+imgs.forEach((item, index) => {
+  console.log(item, index)
+})
 
 let i = 0;
-imgs.forEach( => {
-  console.log(i++);
-});
+imgs.forEach(() => {
+  console.log(i++)
+})
 
-imgs.forEach(() => i++);
+imgs.forEach(() => i++)
 
