a36c27c7d1bdc5bd1488c295eb0b5302aab0baa7
Flávia Reis <flaviarster@gmail.com>
2024-02-12 14:54:46 -0300

    Fiz os exercícios de o que é dom

---
+++ dom-para-iniciantes/o-que-eh-dom/script.js
@@ -0,0 +1,16 @@
+// Retorne o url da página atual utilizando o objeto window
+const url = location.href
+console.log(url)
+
+// Seleciona o primeiro elemento da página que
+// possua a classe ativo
+const ativo = document.querySelector('.ativo')
+console.log(ativo)
+
+// Retorne a linguagem do navegador
+const linguagem = navigator.language
+console.log(linguagem)
+
+// Retorne a largura da janela
+const largura = window.innerWidth
+console.log(largura)
