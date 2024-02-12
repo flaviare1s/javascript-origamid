7dbe72dfe3f270569cbe4ae952975b3584c868dd
Flávia Reis <flaviarster@gmail.com>
2024-02-12 17:05:13 -0300

    Update script.js

--- dom-para-iniciantes/selecao-de-elementos/script.js
+++ dom-para-iniciantes/selecao-de-elementos/script.js
@@ -3,10 +3,13 @@ const imagens = document.querySelectorAll('img')
 console.log(imagens)
 
 // Retorne no console apenas as imagens que começaram com a palavra imagem
-const imagensNome = document.querySelectorAll('img[src^="img/imagem"]')
-console.log(imagensNome)
+const imagensAnimais = document.querySelectorAll('img[src^="img/imagem"]')
+console.log(imagensAnimais)
 
 // Selecione todos os links internos (onde o href começa com #)
+const linksInternos = document.querySelectorAll('a[href^="#"]')
+console.log(linksInternos)
+
 
 // Selecione o primeiro h2 dentro de .animais-descricao
 const primeiroH2 = document.querySelector('.animais-descricao h2')
