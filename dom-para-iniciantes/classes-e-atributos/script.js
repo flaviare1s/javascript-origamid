f16ebc24d2ad0dccb49432f0c59159005616c0f7
Flávia Reis <flaviarster@gmail.com>
2024-02-13 09:05:23 -0300

    Fiz os exercícios de classes e atributos

--- dom-para-iniciantes/classes-e-atributos/script.js
+++ dom-para-iniciantes/classes-e-atributos/script.js
@@ -1,8 +1,31 @@
 // Adicione a classe ativo a todos os itens do menu
+const itensMenu = document.querySelectorAll('.menu a')
+console.log(itensMenu)
 
-// Remove a classe ativo de todos os itens do menu e mantenha apenas no primeiro
+itensMenu.forEach((item) => {
+    item.classList.add('ativo')
+})
+
+
+// Remova a classe ativo de todos os itens do menu e mantenha apenas no primeiro
+itensMenu.forEach((item, index) => {
+    if(index !== 0) {
+        item.classList.remove('ativo')
+    }
+})
+
 
 // Verifique se as imagens possuem o atributo alt
+const imgs = document.querySelectorAll('img')
+
+imgs.forEach((img) => {
+    console.log(img.hasAttribute('alt'))
+})
+
 
 // Modifique o href do link externo no menu
+const linkExterno = document.querySelector('.menu a[href^="https://"]')
+linkExterno.setAttribute('href', 'https://www.google.com')
+
+console.log(linkExterno)
 
