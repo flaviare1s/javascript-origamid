f287d941df26f42b3b94b0cda0edcbe6cd908313
Flávia Reis <flaviarster@gmail.com>
2024-02-15 13:14:00 -0300

    Comecei o capítulo de prototype

--- objetos/prototype/script.js
+++ objetos/prototype/script.js
@@ -3,20 +3,34 @@
 // Crie um método no protótipo que retorne
 // o nome completo da pessoa
 
+function Pessoa(nome, sobrenome, idade) {
+    this.nome = nome
+    this.sobrenome = sobrenome
+    this.idade = idade
+}
+
+Pessoa.prototype.nomeCompleto = function() {
+    return `${this.nome} ${this.sobrenome}`
+}
+
+const pessoa1 = new Pessoa('André', 'Macedo', 31)
+
+console.log(pessoa1.nomeCompleto())
+
 // Liste os métodos acessados por 
 // dados criados com NodeList,
 // HTMLCollection, Document
 
 // Liste os construtores dos dados abaixo
-const li = document.querySelector('li');
+// const li = document.querySelector('li');
 
-li;
-li.click;
-li.innerText;
-li.value;
-li.hidden;
-li.offsetLeft;
-li.click();
+// li;
+// li.click;
+// li.innerText;
+// li.value;
+// li.hidden;
+// li.offsetLeft;
+// li.click();
 
 // Qual o construtor do dado abaixo:
-li.hidden.constructor.name;
+// li.hidden.constructor.name;
