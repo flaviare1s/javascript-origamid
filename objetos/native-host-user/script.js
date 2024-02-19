1aa04a044b639bd558498dfaa42f63115679541f
Flávia Reis <flaviarster@gmail.com>
2024-02-19 10:19:04 -0300

    Fiz exercícios de objetos

--- objetos/native-host-user/script.js
+++ objetos/native-host-user/script.js
@@ -1,6 +1,35 @@
-// Liste 5 objetos nativos
+// Liste 5 objetos nativos.
+// RESPOSTA:
+// Array
+// String
+// Number
+// Object
+// Function
 
 // Liste 5 objetos do browser
+// RESPOSTA:
+// window
+// history
+// Document
+// HTMLCollection
+// NodeList
 
 // Liste 2 Métodos, Propriedades ou Objetos
 // presentes no Chrome que não existem no Firefox
+
+if(typeof document.webkitCancelFullScreen !== 'undefined') {
+    console.log('Existe')
+} else {
+    console.log('existe')
+}
+
+
+if(typeof document.webkitHidden !== 'undefined') {
+    console.log('Existe')
+} else {
+    console.log('existe')
+}
+
+// Ambos esses métodos já existem no Firefox
+
+
