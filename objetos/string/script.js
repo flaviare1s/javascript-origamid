9d3bf2c5da01d3d554d10c4a36f5b549c28ecc2a
Flávia Reis <flaviarster@gmail.com>
2024-02-20 08:50:01 -0300

    Fiz os exercícios de String

--- objetos/string/script.js
+++ objetos/string/script.js
@@ -23,19 +23,49 @@ const transacoes = [
       valor: 'R$ 49',
     },
   ];
+
+  let taxaTotal = 0
+  let recebimentoTotal = 0
+  transacoes.forEach((item) => {
+    const numeroLimpo = +item.valor.replace('R$ ', '')
+    if(item.descricao.includes('Taxa')) {
+      taxaTotal += numeroLimpo
+    } else if(item.descricao.slice('0, 4') === 'Rece') {
+      recebimentoTotal +=numeroLimpo
+    }
+  })
+  console.log(taxaTotal)
+  console.log(recebimentoTotal)
   
   // Retorne uma array com a lista abaixo
   const transportes = 'Carro;Avião;Trem;Ônibus;Bicicleta';
+  const transportesArray = transportes.split(';')
+  console.log(transportesArray)
   
   // Substitua todos os span's por a's
-  const html = `<ul>
+  let html = `<ul>
                   <li><span>Sobre</span></li>
                   <li><span>Produtos</span></li>
                   <li><span>Contato</span></li>
                 </ul>`;
   
+  html = html.split('span').join('a')
+  console.log(html)            
+
   // Retorne o último caracter da frase
   const frase = 'Melhor do ano!';
+  console.log(frase[frase.length - 1])
+  console.log(frase.slice(-1))
   
   // Retorne o total de taxas
-  const transacoes = ['Taxa do Banco', '   TAXA DO PÃO', '  taxa do mercado', 'depósito Bancário', 'TARIFA especial'];
+  const transacoes2 = ['Taxa do Banco', '   TAXA DO PÃO', '  taxa do mercado', 'depósito Bancário', 'TARIFA especial'];
+
+
+  let totalTaxas = 0
+  transacoes2.forEach((item) => {
+    if(item.toLowerCase().includes('taxa')) {
+      return totalTaxas++
+    }
+  })
+
+  console.log(totalTaxas)
