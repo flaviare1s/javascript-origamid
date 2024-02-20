7d4a56ed60404ec3e2c71da94b66112bddb866ad
Flávia Reis <flaviarster@gmail.com>
2024-02-20 10:33:10 -0300

    Fiz os exercícios de Number e Math

--- objetos/number-and-math/script.js
+++ objetos/number-and-math/script.js
@@ -1,13 +1,40 @@
 // Retorne um número aleatório
 // entre 1050 e 2000
 
+// FÓRMULA: Math.floor(Math.random() * (max - min + 1) + min)
+
+const aleatorio = Math.floor(Math.random() * (2000 - 1050 + 1) + 1050)
+console.log(aleatorio)
+
 
 // Retorne o maior número da lista abaixo
 const numeros = '4, 5, 20, 8, 9';
+let arrayNumeros = numeros.split(', ')
+
+console.log(arrayNumeros)
+console.log(...arrayNumeros)
+
+const maximo = Math.max(...arrayNumeros)
+console.log(maximo)
 
 
 // Crie uma função para limpar os preços
 // e retornar os números com centavos arredondados
 // depois retorne a soma total
-const listaPrecos = ['R$ 59,99', ' R$ 100,222',
+let listaPrecos = ['R$ 59,99', ' R$ 100,222',
                      'R$ 230  ', 'r$  200'];
+
+function limparPreco(preco) {
+    preco = +preco.toUpperCase().replace('R$', '').trim().replace(',', '.')
+    preco = +preco.toFixed(2)
+    return preco
+}
+
+let soma = 0
+listaPrecos.forEach((preco) => {
+   soma += limparPreco(preco)
+})
+
+console.log(soma)
+
+console.log(soma.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'}))
