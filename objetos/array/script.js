fec4f992d7c28d998a95176dde122c1ff0bc96e8
Flávia Reis <flaviarster@gmail.com>
2024-02-26 10:31:39 -0300

    Fiz os exercícios de Arrays

--- objetos/array/script.js
+++ objetos/array/script.js
@@ -1,14 +1,32 @@
-const comidas = ['Pizza', 'Frango', 'Carne', 'Macarrão'];
+const comidas = ['Pizza', 'Feijão', 'Legumes', 'Macarrão'];
 // Remova o primeiro valor de comidas e coloque em uma variável
+const primeiro = comidas.shift()
+console.log(primeiro)
 // Remova o último valor de comidas e coloque em uma variável
+const ultimo = comidas.pop()
+console.log(ultimo)
 // Adicione 'Arroz' ao final da array
-// Adicione 'Peixe' e 'Batata' ao início da array
+comidas.push('Arroz')
+// Adicione 'Sanduíche' e 'Batata' ao início da array
+comidas.unshift('Sanduíche', 'Batata')
+
+console.log(comidas)
+
+
 
 const estudantes = ['Marcio', 'Brenda', 'Joana', 'Kleber', 'Julia'];
 // Arrume os estudantes em ordem alfabética
+estudantes.sort()
 // Inverta a ordem dos estudantes
+estudantes.reverse()
 // Verifique se Joana faz parte dos estudantes
+console.log(estudantes.includes('Joana'))
 // Verifique se Juliana faz parte dos estudantes
+console.log(estudantes.includes('Juliana'))
+
+console.log(estudantes)
+
+
 
 let html = `<section>
               <div>Sobre</div>
@@ -17,7 +35,18 @@ let html = `<section>
             </section>`
 // Substitua section por ul e div com li,
 // utilizando split e join
+const ul = html.split('section').join('ul').split('div').join('li')
+
+console.log(ul)
+
 
 const carros = ['Ford', 'Fiat', 'VW', 'Honda'];
 // Remova o último carro, mas antes de remover
 // salve a array original em outra variável
+
+const carros2 = carros.slice()
+
+carros.pop()
+
+console.log(carros2)
+
