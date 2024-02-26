63f7f36fe8d0ff0f13485ab17f6d2ce1c6f40290
Flávia Reis <flaviarster@gmail.com>
2024-02-26 18:34:58 -0300

    Terminei os exercícios de iteração de arrays

--- objetos/array-e-iteracao/script.js
+++ objetos/array-e-iteracao/script.js
@@ -1,6 +1,21 @@
-// Selecione cada curso e retorne uma array
-// com objetos contendo o título, descricao,
-// aulas e horas de cada curso
+// Selecione cada curso e retorne uma array com objetos contendo o título, descricao, aulas e horas de cada curso
+const cursos = document.querySelectorAll('.curso')
+const arrayCursos = Array.from(cursos)
+
+const objetosCurso = arrayCursos.map ((curso) => {
+  const titulo = curso.querySelector('h1').innerText
+  const descricao = curso.querySelector('p').innerText
+  const aulas = curso.querySelector('.aulas').innerText
+  const horas = curso.querySelector('.horas').innerText
+  return {
+    titulo,
+    descricao,
+    aulas,
+    horas
+  }
+})
+
+console.log(objetosCurso)
 
 
 // Retorne uma lista com os
@@ -19,6 +34,12 @@ console.log(maioresQue100)
 const instrumentos = ['Guitarra', 'Baixo', 'Bateria', 'Teclado']
 
 console.log(instrumentos.includes('Baixo'))
+
+const possuiBaixo = instrumentos.some((item) => {
+  return item === 'Baixo'
+})
+
+console.log(possuiBaixo)
 
 
 // Retorne o valor total das compras
@@ -45,24 +66,35 @@ const compras = [
   }
 ]
 
-const precos = compras.map((item) => {
-  return item.preco
-})
-
-console.log(precos)
-
-const precosNumero = precos.map((preco) => {
-  return preco.replace(',', '.')
-})
-
-const precosNumeroFloat = precosNumero.map((preco) => {
-  return parseFloat(preco.split('R$')[1])
-})
-
-console.log(precosNumeroFloat)
-
-const valorTotal = precosNumeroFloat.reduce((acc, preco) => {
-  return acc += preco
-}, 0)
+const valorTotal = compras.reduce((acc, item) => {
+  const precoLimpo = +item.preco.replace('R$', '').replace(',', '.')
+  return acc + precoLimpo
+},0)
 
 console.log(valorTotal)
+
+
+// Outra maneira menos otimizada:
+
+// const precos = compras.map((item) => {
+//   return item.preco
+// })
+
+// console.log(precos)
+
+// const precosNumero = precos.map((preco) => {
+//   return preco.replace(',', '.')
+// })
+
+// const precosNumeroFloat = precosNumero.map((preco) => {
+//   return parseFloat(preco.split('R$')[1])
+// })
+
+// console.log(precosNumeroFloat)
+
+// const valorTotal = precosNumeroFloat.reduce((acc, preco) => {
+//   return acc + preco
+// }, 0)
+
+//console.log(valorTotal)
+
