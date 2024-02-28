3e6195aa5732ac1f9aeaf8f61883d5c0e996faf3
Flávia Reis <flaviarster@gmail.com>
2024-02-28 11:03:29 -0300

    Dividi o projeto em modules e instalei o Vite, pois não estava conseguindo rodar com o live server.

---
+++ efeitos-no-dom/js/script.js
@@ -0,0 +1,10 @@
+import { initScrollSuave } from "./modules/scroll-suave";
+import { initAnimacaoScroll } from "./modules/animacao-por-scroll";
+import { initAccordion } from "./modules/animacao-accordion";
+import { initTabNav } from "./modules/animacao-tab-nav";
+
+initScrollSuave()
+initAnimacaoScroll()
+initAccordion()
+initTabNav()
+
