bab06be02a01a343720052911b87a5b6dcd13c2d
Flávia Reis <flaviarster@gmail.com>
2024-02-29 09:49:31 -0300

    Terminei o menu dropdown

--- efeitos-no-dom/js/modules/dropdown-menu.js
+++ efeitos-no-dom/js/modules/dropdown-menu.js
@@ -1,13 +1,39 @@
 export function initDropdown() {
     const dropdownMenus = document.querySelectorAll('[data-dropdown]')
 
-    function handleClick(event) {
-        event.preventDefault()
-        this.classList.toggle('active')
-    }
-
+    
     dropdownMenus.forEach((menu) => {
         menu.addEventListener('touchstart', handleClick)
         menu.addEventListener('click', handleClick)
     })
+
+    function handleClick(event) {
+        event.preventDefault()
+        this.classList.add('active')
+        outsideClick(this, ['click', 'touchstart'], () => {
+            this.classList.remove('active')
+        })
+    }
+
+    function outsideClick(element, events, callback) {
+        const html = document.documentElement
+        const outside = 'data-outside'
+
+        if(!element.hasAttribute(outside)) {
+            events.forEach(userEvent => {
+                html.addEventListener(userEvent, handleOutsideClick)
+            })
+            element.removeAttribute(outside, '')
+        }
+        
+        function handleOutsideClick(event) {
+            if(!element.contains(event.target)) {
+                element.removeAttribute(outside)
+                events.forEach(userEvent => {
+                    html.removeEventListener(userEvent, handleOutsideClick)
+                })
+                callback()
+            }
+        }
+    }
 }
