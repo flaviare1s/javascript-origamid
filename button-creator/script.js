84a4e7cf4e67430c5b866e36f19464e9e1c87a5c
Flávia Reis <flaviarster@gmail.com>
2024-03-03 14:58:30 -0300

    Desenvolvimento do js do button creator

---
+++ button-creator/script.js
@@ -0,0 +1,48 @@
+const controles = document.getElementById('controles')
+const cssText = document.querySelector('.css')
+const btn = document.querySelector('.btn')
+
+const handleStyle = {
+    element: btn,
+    texto(value) {
+        this.element.innerText = value
+    },
+    color(value) {
+        this.element.style.color = value
+    },
+    backgroundColor(value) {
+        this.element.style.backgroundColor = value
+    },
+    height(value) {
+        this.element.style.height = value + 'px'
+    },
+    width(value) {
+        this.element.style.width = value + 'px'
+    },
+    border(value) {
+        this.element.style.border = value 
+    },
+    borderRadius(value) {
+        this.element.style.borderRadius = value + 'px'
+    },
+    fontFamily(value) {
+        this.element.style.fontFamily = value 
+    },
+    fontSize(value) {
+        this.element.style.fontSize = value + 'rem'
+    }
+}
+
+function handleChange(event) {
+    const name = event.target.name
+    const value = event.target.value
+
+    handleStyle[name](value)
+    showCSS()
+}
+controles.addEventListener('change', handleChange)
+
+function showCSS() {
+    cssText.innerHTML = '<span>' + btn.style.cssText.split('; ').join(';</span><span>')
+}
+
