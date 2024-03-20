d950f20bda5dc1779b750fd2008dbe41e68ac7d1
Flávia Reis <flaviarster@gmail.com>
2024-03-20 10:55:58 -0300

    Criei as pastas do proximo capítulo e adicionei os exercícios

---
+++ mais-javascript/factory-function/script.js
@@ -0,0 +1,14 @@
+function createButton(text) {
+    function element() {
+      const buttonElement = document.createElement('button');
+      buttonElement.innerText = text;
+      return buttonElement;
+    }
+    return {
+      element: element,
+      text: text,
+    }
+  }
+  
+  const comprarBtn = createButton('Comprar')
+
