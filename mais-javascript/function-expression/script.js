ccd8851ae29b9006f3090fe38cedacd74705432d
Flávia Reis <flaviarster@gmail.com>
2024-03-31 09:57:37 -0300

    Fiz os exercícios de function expression

--- mais-javascript/function-expression/script.js
+++ mais-javascript/function-expression/script.js
@@ -1,11 +1,21 @@
-// Remova o erro
-priceNumber('R$ 99,99');
-const priceNumber = n => +n.replace('R$', '').replace(',', '.');
+// Remova o erro:
+// priceNumber('R$ 99,99');
+// const priceNumber = n => +n.replace('R$', '').replace(',', '.');
+// R- O erro está na tentativa de inicializar uma function expression antes de declará-la. O certo seria:
+const priceNumber = n => +n.replace('R$', '').replace(',', '.')
+console.log(priceNumber('R$ 99,99'))
 
-// Crie uma IIFE e isole o escopo
-// de qualquer código JS.
 
-// Como podemos utilizar
-// a função abaixo.
-const active = callback => callback();
+// Crie uma IIFE e isole o escopo de qualquer código JS.
+// (function() {
+//     const nome = 'teste'
+//     console.log(nome)
+// })()
+
+// Como podemos utilizar a função abaixo.
+const active = callback => callback()
+
+active(function() {
+    console.log('Testando active')
+})
 
